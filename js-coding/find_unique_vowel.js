465c37b57d6147ae876c2101be55bf6aa020723a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-04-18 08:31:37 +0530

    add abb interview exp

--- js-coding/find_unique_vowel.js
+++ js-coding/find_unique_vowel.js
@@ -1,22 +1,29 @@
-function findVowels(str) {
-    const listOfVowels = ['a', 'e', 'i', 'o', 'u'];
-    const splittedStr = str.split('');
+function findVowels(inputString) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const splittedInputStr = inputString.split('');
     const vowelFound = [];
     let uniqueVowelFound = new Set();
-    if (splittedStr && splittedStr.length > 0) {
-        splittedStr.forEach((char) => {
+
+    if (splittedInputStr && splittedInputStr.length > 0) {
+        splittedInputStr.forEach((char) => {
             const checkChar = char ? char.toLowerCase() : '';
-            console.log(char + ' is vowel => ', listOfVowels.includes(checkChar));
-            if (listOfVowels.includes(checkChar)) {
+            console.log(char + ' is vowel => ', vowels.includes(checkChar));
+            if (vowels.includes(checkChar)) {
                 vowelFound.push(char);
             }
         });
     }
-    console.log("Vowels found in " + str + " is/are ", vowelFound.join(','));
+    console.log("Vowels found in " + inputString + " is/are ", vowelFound.join(','));
     uniqueVowelFound = new Set(vowelFound);
-    console.log("Unique Vowels found in " + str + " is/are ", Array.from(uniqueVowelFound));
+    console.log("Unique Vowels found in " + inputString + " is/are ", Array.from(uniqueVowelFound));
 }
 
 
 findVowels("Saikat Mahapatra");
 //https://jsfiddle.net/saikatmahapatra/ycLpf6k3/18/
+
+/*for (let char of inputString) {
+    if (vowels.includes(char)) {
+        result.push(char);
+    }
+}*/
